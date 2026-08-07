a8e697f2776d7d42d19d6310b93ce1ec1a4baf38
slavonixi <salvatorefidone29@gmail.com>
2026-08-07 18:23:01 +0200

    chore(dev-tools): install django_exceptions

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'inventory.apps.InventoryConfig',
     'rest_framework',
+    'django_extensions',
     'django_celery_results',
     'django.contrib.admin',
     'django.contrib.auth',
