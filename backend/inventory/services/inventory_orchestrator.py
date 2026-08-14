e0becb9a4a3643ee6a2318856bda8febafc8706b
slavonixi <salvatorefidone29@gmail.com>
2026-08-14 11:40:14 +0200

    feat(inventory): add validations in orchestrator
    - add validate_movement_update():
        - validates movement_item
        - validates inventory_item's operation update (calculates delta between old_qty and new_qty)
    - add validate_movement_create():
        - validates brand-new movement_item
        - validates inventory_item' stock (qty) update

--- backend/inventory/services/inventory_orchestrator.py
+++ backend/inventory/services/inventory_orchestrator.py
@@ -1,10 +1,10 @@
 from inventory.services.inventory_services import InventoryServices
 from inventory.services.movement_services import MovementServices
-from api import exceptions
+from backend.inventory import exceptions
 from django.db import transaction
 from api import tasks
 from celery import Celery
-from api import exceptions
+from backend.inventory import exceptions
 import traceback
 import sys
 from rest_framework.exceptions import APIException #type: ignore
@@ -19,4 +19,19 @@ class InventoryOrchestrator:
             both the Movement item and the Inventory item.
     """ 
 
-    
+    #############################
+    #     Validation Method     #
+    #############################
+    @staticmethod
+    def validate_movement_update(inventory_item, movement_item, old_qty):
+
+        new_qty = movement_item.qty
+        MovementServices.validate_movement_item(movement_item)
+        InventoryServices.validate_stock_operation_update(inventory_item, old_qty, new_qty)
+
+    @staticmethod
+    def validate_movement_create(inventory_item, movement_item):
+
+        qty = movement_item.qty
+        MovementServices.validate_movement_item(movement_item)
+        InventoryServices.validate_stock_operation(inventory_item, qty)
